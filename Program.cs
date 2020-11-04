fa725a4ee2d6046693773980bfae7d94a9496bf3
maixuandiep810 <maixuandiep810@gmail.com>
2020-11-04 22:35:27 +0700

    using generic Action, Func, ...

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace helloGeneric
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Car firstCar = new Car("Audi");
+            firstCar.printCarByGeneric();
+        }
+    }
+
+    class VehicleHepler
+    {
+        public static void heluGeneric(Action _printNameDelegate) 
+        {
+            _printNameDelegate.Invoke();
+        }
+    }
+
+    class Car 
+    {
+        private string _Name { get; set; }
+
+        public Car(string _name)
+        {
+            _Name = _name;
+        }
+
+        public void printCar()
+        {
+            Console.WriteLine(_Name);
+        }
+
+        public void printCarByGeneric()
+        {
+            VehicleHepler.heluGeneric(printCar);
+        }
+    }
+}
